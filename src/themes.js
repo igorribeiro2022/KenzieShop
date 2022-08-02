823d5404140cd48c6e72ccfaf8ddba191bf9dde2
igorribeiro2022 <igor.rogerio.ribeiro@gmail.com>
2022-08-02 18:48:52 -0300

    refactor: remove comments

--- src/themes.js
+++ src/themes.js
@@ -1,4 +1,3 @@
-// Objeto com a definição do estilo para o tema lightTheme
 const lightTheme = {
     color: "#0e14e",
     backgroundColor: "rgb(0,0,0,0.1)",
@@ -9,20 +8,17 @@ const lightTheme = {
     },
   };
   
-  // Objeto com a definição do estilo para o tema darkTheme
   const darkTheme = {
     color: "#D3D4E6",
     backgroundColor: "rgb(0,0,0,0.9)",
+
     button: {
       textColor: "white",
       borderColor: "#151A69",
       backgroundColor: "#151A69",
     },
   };
-  
-  // Exportando as definições de estilo (lightTheme e darkTheme)
-  // no objeto themes, que será usado na props theme do 
-  // componente auxiliar <ThemeProvider>
+
   export const themes = {
     light: lightTheme,
     dark: darkTheme,
